7150a3fa1d6207271dd11487b38b20f137f026aa
Stukova Olya <stukova.o@gmail.com>
2026-02-04 12:55:33 +0500

    Add Storybook for documentation and interactive demos

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from '@storybook/html-vite'
+import { themes } from 'storybook/theming'
+
+import './style.css'
+
+const preview: Preview = {
+  parameters: {
+    layout: 'fullscreen',
+    controls: {
+      disable: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+      codePanel: true,
+      canvas: {
+        sourceState: 'shown',
+      },
+    },
+  actions:  { disable: true },
+  interactions: { disable: true },
+  },
+}
+
+export default preview
